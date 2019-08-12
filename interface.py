8b5da00e8b169e5fd20ca8217308c5abb0ccad29
Aviel Resnick <aviel.resnick@gmail.com>
2019-08-12 12:03:21 -0400

    Day 8.2: Implemented segmentation into the UI
    Loading from file currently broken.

--- interface.py
+++ interface.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import tkinter.filedialog
+import tkinter.simpledialog
 from PIL import ImageTk, Image
 from tkinter import Tk, Text, BOTH, W, N, E, S
 from tkinter.ttk import Frame, Button, Label, Style
 import os.path
 from os import path
 import webbrowser
+import unsupervisedSeg
+pathToImg = None
+configFile = None
 
 class Segmentation(Frame):
     def __init__(self):
@@ -13,8 +17,9 @@ class Segmentation(Frame):
         self.initUI()
 
     def selectImage(self):
-        path = tkinter.filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
-        img = Image.open(path)
+        global pathToImg
+        pathToImg = tkinter.filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
+        img = Image.open(pathToImg)
         photoImg = ImageTk.PhotoImage(img)
         width, height = img.size
         resX = width + 150
@@ -26,11 +31,24 @@ class Segmentation(Frame):
         panel.grid(row=1, column=0, columnspan=2, rowspan=5, padx=5, sticky=E+W+S+N)
 
     def tune(self):
-        if path.exists("config.txt"):
-            webbrowser.open("config.txt")
+        global configFile
+        configFile = "config.txt" # default path
 
-        else:
+        # TODO: Parse text to prevent unwanted commands from executing
+        while not path.exists(configFile):
             print("we don't have a config file")
+            configFile = tkinter.simpledialog.askstring("Config Path", "Path to config.txt")
+
+        webbrowser.open(configFile)
+
+    def calibration(self):
+        inputDialog = tkinter.simpledialog.askstring("Calibration", "1mm = ?px")
+        print(inputDialog)
+    
+    def segment(self):
+        inputFile = pathToImg
+        print(inputFile)
+        unsupervisedSeg.main(inputFile, configFile)
 
     def initUI(self):
         self.master.title("Segmentation")
@@ -51,10 +69,10 @@ class Segmentation(Frame):
         btnTune = Button(self, text="Manual Tuning", command = self.tune)
         btnTune.grid(row=2, column=3, padx=10, pady=20)
 
-        btnCalibrate = Button(self, text="Calibration")
+        btnCalibrate = Button(self, text="Calibration", command = self.calibration)
         btnCalibrate.grid(row=3, column=3, padx=10, pady=20)
 
-        btnSegment = Button(self, text="Train & Segment")
+        btnSegment = Button(self, text="Train & Segment", command = self.segment)
         btnSegment.grid(row=4, column=3, padx=10, pady=20)
 
         btnPartition = Button(self, text="Assign Partitions")
--- unsupervisedSeg.py
+++ unsupervisedSeg.py
@@ -12,6 +12,8 @@ from skimage import segmentation
 import torch.nn.init
 import random
 
+args = 0
+
 #ogImage = cv2.imread("184-6.jpg")
 #segmented = cv2.imread("184-6 Output.png")
 #labelColors = [[255,225,25],[0,130,200],[245,130,48],[250,190,190],[230,190,255],[128,0,0],[0,0,128],[128,128,128],[255,255,255],[0,0,0]]
@@ -24,28 +26,11 @@ import random
 
 #preSegGray = cv2.cvtColor(preSeg, cv2.COLOR_BGR2GRAY)
 
-cudaAvailable = torch.cuda.is_available()
-
 # class to pull configuration arguments from a seperate file
 class LoadFromFile (argparse.Action):
     def __call__ (self, parser, namespace, values, option_string = None):
         with values as f:
             parser.parse_args(f.read().split(), namespace)
-
-# parser setup
-parser = argparse.ArgumentParser(description='Unsupervised Arterial Segmentation ')
-parser.add_argument('--numChannels', metavar='N', default=100, type=int, help='number of channels')
-parser.add_argument('--maxIter', metavar='T', default=1000, type=int, help='number of maximum iterations')
-parser.add_argument('--minLabels', metavar='minL', default=5, type=int, help='minimum number of labels')
-parser.add_argument('--lr', metavar='LR', default=0.1, type=float, help='learning rate')
-parser.add_argument('--numConv', metavar='M', default=2, type=int, help='number of convolutional layers')
-parser.add_argument('--numSuperpixels', metavar='K', default=10000, type=int, help='number of superpixels')
-parser.add_argument('--compactness', metavar='C', default=100, type=float, help='compactness of superpixels')
-parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int, help='visualization flag')
-parser.add_argument('--sigma', metavar='S', default=0, type=float, help='gaussian smoothing')
-parser.add_argument('--input', metavar='FILENAME', help='input image file name', required=False)
-parser.add_argument('--file', type=open, action=LoadFromFile)
-args = parser.parse_args()
 
 # CNN model
 class CNN(nn.Module):
@@ -73,102 +58,6 @@ class CNN(nn.Module):
         x = self.bn3(x)
         return x
 
-# load image
-image = cv2.imread(args.input)
-
-# scale images back down (divide by 255)
-# changes format from NHWC --> NCWH 
-data = torch.from_numpy(np.array([image.transpose((2, 0, 1)).astype('float32')/255.0]))
-
-# TEMP: checks cuda avaliablity
-print(cudaAvailable)
-
-if cudaAvailable:
-    data = data.cuda()
-
-data = Variable(data)
-
-# Segments image using k-means clustering in Color-(x,y,z) space.
-labels = segmentation.slic(image, compactness=args.compactness, sigma=args.sigma, n_segments=args.numSuperpixels)
-labels = labels.reshape(image.shape[0]*image.shape[1])
-uniqueLabels = np.unique(labels)
-l_inds = []
-for i in range(len(uniqueLabels)):
-    l_inds.append(np.where(labels == uniqueLabels[i])[0])
-
-# train
-model = CNN(data.size(1))
-if cudaAvailable:
-    model.cuda()
-model.train()
-
-lossFunction = torch.nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9) # stochastic gradient descent 
-
-# generates a 100 x 3 list of integers up to 255 (100 different colors)
-# label_colours = np.random.randint(255, size=(100,3))
-
-labelColors = [[255,225,25],[0,130,200],[245,130,48],[250,190,190],[230,190,255],[128,0,0],[0,0,128],[128,128,128],[255,255,255],[0,0,0]]
-
-print(labelColors)
-
-# optimizes maxIter times
-for batchIndex in range(args.maxIter):
-    # forwarding
-    optimizer.zero_grad()
-    output = model(data)[0]
-    output = output.permute(1, 2, 0).contiguous().view(-1, args.numChannels)
-    _, target = torch.max(output, 1)
-    imTarget = target.data.cpu().numpy()
-    nLabels = len(np.unique(imTarget))
-
-    # display image
-    if args.visualize:
-        # colors in segments
-        imTargetRGB = np.array([labelColors[c % 10] for c in imTarget])
-        imTargetRGB = imTargetRGB.reshape(image.shape).astype(np.uint8)
-        cv2.imshow("Result", imTargetRGB)
-        cv2.waitKey(10)
-
-    # superpixel refinement
-    # TODO: use Torch Variable instead of numpy for faster calculation
-    for i in range(len(l_inds)):
-        labels_per_sp = imTarget[l_inds[i]]
-        u_labels_per_sp = np.unique(labels_per_sp)
-        hist = np.zeros(len(u_labels_per_sp))
-        for j in range(len(hist)):
-            hist[j] = len(np.where(labels_per_sp == u_labels_per_sp[j])[0])
-        imTarget[l_inds[i]] = u_labels_per_sp[np.argmax(hist)]
-    
-    target = torch.from_numpy(imTarget)
-    if cudaAvailable:
-        target = target.cuda()
-
-    target = Variable(target)
-    loss = lossFunction(output, target)
-    loss.backward()
-    optimizer.step()
-
-    # print (batch_idx, '/', args.maxIter, ':', nLabels, (loss.data[0]))
-    
-    # for pytorch 1.0
-    # print (batch_idx, '/', args.maxIter, ':', nLabels, loss.item())
-    if nLabels <= args.minLabels:
-        print ("nLabels", nLabels, "reached minLabels", args.minLabels, ".")
-        break
-
-if not args.visualize:
-    output = model(data)[0]
-    output = output.permute(1, 2, 0).contiguous().view(-1, args.numChannels)
-    _, target = torch.max(output, 1)
-    imTarget = target.data.cpu().numpy()
-    imTargetRGB = np.array([labelColors[c % 10] for c in imTarget])
-    imTargetRGB = imTargetRGB.reshape(image.shape).astype(np.uint8)
-    
-# save output image
-outputName = str(args.input).split('.')[0] + " Output.png"
-cv2.imwrite(outputName, imTargetRGB)
-
 def display(windowName, window, notableContours):
     for contour in notableContours:
         cv2.drawContours(window, contour, -1, (0, 0, 255), 3)
@@ -177,28 +66,146 @@ def display(windowName, window, notableContours):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-for i in labelColors:
-    imageContoured = image.copy()
-    notableContours = []
+def main(inputImage, configFile):
+    global args
+    cudaAvailable = torch.cuda.is_available()
 
-    lowerLim = np.array(i)
-    upperLim = np.array(i)
+    # parser setup
+    parser = argparse.ArgumentParser(description='Unsupervised Arterial Segmentation ')
+    parser.add_argument('--numChannels', metavar='N', default=100, type=int, help='number of channels')
+    parser.add_argument('--maxIter', metavar='T', default=10, type=int, help='number of maximum iterations')
+    parser.add_argument('--minLabels', metavar='minL', default=5, type=int, help='minimum number of labels')
+    parser.add_argument('--lr', metavar='LR', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--numConv', metavar='M', default=2, type=int, help='number of convolutional layers')
+    parser.add_argument('--numSuperpixels', metavar='K', default=10000, type=int, help='number of superpixels')
+    parser.add_argument('--compactness', metavar='C', default=100, type=float, help='compactness of superpixels')
+    parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int, help='visualization flag')
+    parser.add_argument('--sigma', metavar='S', default=0, type=float, help='gaussian smoothing')
+    parser.add_argument('--input', metavar='FILENAME', help='input image file name', required=False)
+    parser.add_argument('--file', type=open, action=LoadFromFile)
 
-    mask = cv2.inRange(imTargetRGB, lowerLim, upperLim)
-    returned, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    args = parser.parse_args()
+    # load image
+    image = cv2.imread(inputImage)
 
-    colorExists = False
+    # scale images back down (divide by 255)
+    # changes format from NHWC --> NCWH 
+    data = torch.from_numpy(np.array([image.transpose((2, 0, 1)).astype('float32')/255.0]))
 
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        arc = cv2.arcLength(contour, False)
-        if area > 5000:
-            print("we have a", area, "contour")
-            notableContours.append(contour)
-            colorExists = True
+    # TEMP: checks cuda avaliablity
+    print(cudaAvailable)
 
-    outputName = ("Contour "+str(i)+" .png")
+    if cudaAvailable:
+        data = data.cuda()
+
+    data = Variable(data)
+
+    # Segments image using k-means clustering in Color-(x,y,z) space.
+    labels = segmentation.slic(image, compactness=args.compactness, sigma=args.sigma, n_segments=args.numSuperpixels)
+    labels = labels.reshape(image.shape[0]*image.shape[1])
+    uniqueLabels = np.unique(labels)
+    l_inds = []
+    for i in range(len(uniqueLabels)):
+        l_inds.append(np.where(labels == uniqueLabels[i])[0])
+
+    # train
+    model = CNN(data.size(1))
+    if cudaAvailable:
+        model.cuda()
+    model.train()
+
+    lossFunction = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9) # stochastic gradient descent 
+
+    # generates a 100 x 3 list of integers up to 255 (100 different colors)
+    # label_colours = np.random.randint(255, size=(100,3))
+
+    labelColors = [[255,225,25],[0,130,200],[245,130,48],[250,190,190],[230,190,255],[128,0,0],[0,0,128],[128,128,128],[255,255,255],[0,0,0]]
+
+    print(labelColors)
+
+    # optimizes maxIter times
+    for batchIndex in range(args.maxIter):
+        # forwarding
+        optimizer.zero_grad()
+        output = model(data)[0]
+        output = output.permute(1, 2, 0).contiguous().view(-1, args.numChannels)
+        _, target = torch.max(output, 1)
+        imTarget = target.data.cpu().numpy()
+        nLabels = len(np.unique(imTarget))
+
+        # display image
+        if args.visualize:
+            # colors in segments
+            imTargetRGB = np.array([labelColors[c % 10] for c in imTarget])
+            imTargetRGB = imTargetRGB.reshape(image.shape).astype(np.uint8)
+            cv2.imshow("Result", imTargetRGB)
+            cv2.waitKey(10)
+
+        # superpixel refinement
+        # TODO: use Torch Variable instead of numpy for faster calculation
+        for i in range(len(l_inds)):
+            labels_per_sp = imTarget[l_inds[i]]
+            u_labels_per_sp = np.unique(labels_per_sp)
+            hist = np.zeros(len(u_labels_per_sp))
+            for j in range(len(hist)):
+                hist[j] = len(np.where(labels_per_sp == u_labels_per_sp[j])[0])
+            imTarget[l_inds[i]] = u_labels_per_sp[np.argmax(hist)]
+        
+        target = torch.from_numpy(imTarget)
+        if cudaAvailable:
+            target = target.cuda()
+
+        target = Variable(target)
+        loss = lossFunction(output, target)
+        loss.backward()
+        optimizer.step()
+
+        # print (batch_idx, '/', args.maxIter, ':', nLabels, (loss.data[0]))
+        
+        # for pytorch 1.0
+        # print (batch_idx, '/', args.maxIter, ':', nLabels, loss.item())
+        if nLabels <= args.minLabels:
+            print ("nLabels", nLabels, "reached minLabels", args.minLabels, ".")
+            break
+
+    if not args.visualize:
+        output = model(data)[0]
+        output = output.permute(1, 2, 0).contiguous().view(-1, args.numChannels)
+        _, target = torch.max(output, 1)
+        imTarget = target.data.cpu().numpy()
+        imTargetRGB = np.array([labelColors[c % 10] for c in imTarget])
+        imTargetRGB = imTargetRGB.reshape(image.shape).astype(np.uint8)
+        
+    # save output image
+    outputName = str(inputImage).split('.')[0] + " Output.png"
+    cv2.imwrite(outputName, imTargetRGB)
+
+    for i in labelColors:
+        imageContoured = image.copy()
+        notableContours = []
     
-    if colorExists:
-        display(outputName, imageContoured, notableContours)
+        lowerLim = np.array(i)
+        upperLim = np.array(i)
+    
+        mask = cv2.inRange(imTargetRGB, lowerLim, upperLim)
+        returned, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    
+        colorExists = False
+    
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            arc = cv2.arcLength(contour, False)
+            if area > 5000:
+                print("we have a", area, "contour")
+                notableContours.append(contour)
+                colorExists = True
+    
+        outputName = ("Contour "+str(i)+" .png")
+        
+        if colorExists:
+            display(outputName, imageContoured, notableContours)
+
+if __name__ == '__main__':
+    main()
 
